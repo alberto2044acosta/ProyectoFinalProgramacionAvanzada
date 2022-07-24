9e955a2fab829335110f194527e3d666568a6cee
Alberto Acosta <albertoacosta1007@gmail.com>
2022-07-24 03:39:08 -0400

    Importando clases.

--- src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/services/homework11/PagosSaga.java
+++ src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/services/homework11/PagosSaga.java
@@ -1,4 +1,22 @@
 package edu.programacion.avanzada.albertoacosta.ProyectoFinal.services.homework11;
 
+
+import edu.programacion.avanzada.albertoacosta.ProyectoFinal.services.homework11.procesos.CancelarPago;
+import edu.programacion.avanzada.albertoacosta.ProyectoFinal.services.homework11.procesos.CrearPago;
+import edu.programacion.avanzada.albertoacosta.ProyectoFinal.services.homework11.procesos.RembolsoDePago;
+
 public class PagosSaga {
+
+    private final CancelarPago cancelarPago() {
+        return cancelarPago();
+    }
+
+    private final CrearPago crearPago() {
+        return crearPago();
+    }
+
+    private final RembolsoDePago rembolsoDePago() {
+        return rembolsoDePago();
+
+    }
 }
--- src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/services/homework11/procesos/CancelarPago.java
+++ src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/services/homework11/procesos/CancelarPago.java
@@ -1,4 +1,6 @@
 package edu.programacion.avanzada.albertoacosta.ProyectoFinal.services.homework11.procesos;
 
 public class CancelarPago {
+
+    private String name;
 }
