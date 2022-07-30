8bb6985999c5f302f5b4cd34d39bc995f3bc4ecc
Alberto Acosta <albertoacosta1007@gmail.com>
2022-07-29 22:38:55 -0400

    Incluyendo comentarios.

--- src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/ProyectoFinalApplication.java
+++ src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/ProyectoFinalApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoFinalApplication {
 
+	/*El programa sigue conteniendo errores a la hora de compilar:
+
+	1-CQRSConfiguration (se encuentra en configuration)
+	2-En resources aparece gris en vez de naranja
+
+	No sé por qué sucede, están escritos de la misma manera que en los videos
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
 	}
--- src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/configuration/CQRSConfiguration.java
+++ src/main/java/edu/programacion/avanzada/albertoacosta/ProyectoFinal/configuration/CQRSConfiguration.java
@@ -1,21 +1,25 @@
 package edu.programacion.avanzada.albertoacosta.ProyectoFinal.configuration;
 
+//TODO esta clase esta igual que en los videos, mas no funciona
+
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.integration.channel.PublishSubscribeChannel;
-//import org.springframework.integration.core.MessagingTemplate;
-//import org.springframework.messaging.MessageChannel;
+import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.MessageChannel;
 
 @Configuration
 public class CQRSConfiguration {
 
-    //@Bean
-    //public MessagingTemplate messagingTemplate() {
-   //     return new MessagingTemplate();
-    //}
+    @Bean
+    public MessagingTemplate messagingTemplate() {
+        return new MessagingTemplate();
+    }
 
-    //@Bean
-   // public MessageChannel commandChannel() {
-    //    return new PublishSubscribeChannel();
-    //}
+    @Bean
+    public MessageChannel commandChannel() {
+        return new PublishSubscribeChannel();
+    }
 }
+ */
